d77f7a4226203e11af12130908bc9b4d74769aeb
Joe Orr <joeorr4@gmail.com>
2012-11-12 17:56:31 -0800

    added more lab 8 boilerplate

--- lab8/spi.h
+++ lab8/spi.h
@@ -1,20 +1,20 @@
 #ifndef _SPI_H
 #define _SPI_H
 
-//These are the procedures you need to write.  Only two are called directly from the main module: 
+//These are the procedures you need to write. Only two are called directly from the main module: 
 
 //initializes the SPI module.  It does not need to set the SPI clock speed.
 void spiinit();
 
 /*  returns the status of the CD signal from the microSD card adapter.
     It should return true (non-zero) when there is a card in the adapter. */
-uint8_t spicardpresent();
+uint8 spicardpresent();
 
 /*  The remaining procedures are called from the SD card module (note, these procedures all have 
     underscores in their names) */
 
 //sets the SPI clock speed.  If spd is 0, the clock is set to approximately 400KHz; otherwise the clock is set to maximum speed.
-void spi_set_divisor(uint8_t spd);
+void spi_set_divisor(uint8 spd);
 
 //asserts the CS chip select signal line (sets it low).
 void spi_cs_assert();
@@ -23,12 +23,12 @@ void spi_cs_assert();
 void spi_cs_deassert();
 
 //sends one byte over the SPI bus.
-void spi_send_byte(uint8_t input);
+void spi_send_byte(uint8 input);
 
 //receives one byte over the SPI bus
-uint8_t spi_rcv_byte();
+uint8 spi_rcv_byte();
 
 //sends multiple bytes (normally a SD card sector) over the SPI bus.  The code for this procedure was given earlier.
-void spi_rcv_buffer(uint16_t len, uint8_t xdata *buffer);
+void spi_rcv_buffer(uint16 len, uint8 xdata *buffer);
 
 #endif
