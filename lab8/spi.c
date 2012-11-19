a63500c5061030342c00dbab4fc6fa9d9faa8083
Aseda Gyeke Aboagye <aseda.aboagye@gmail.com>
2012-11-18 19:51:41 -0800

    Prestidigitaiton.

--- lab8/spi.c
+++ lab8/spi.c
@@ -21,21 +21,31 @@
 #define CHKB(bit)   (bit##_PORT & (1 << bit##_POS))     // chk dat b
                                                         // where b == bit.
 
+sbit CS     = P2^0;
+sbit CD     = P2^1;
+sbit DO     = P0^1;
+sbit DI     = P0^2;
+sbit SCK    = P0^0;
+
 //initializes the SPI module.  It does not need to set the SPI clock speed.
 void spiinit(void){
     SFRPAGE = 0;
     SPI0CFG |= 0x40;        //Enable master mode. (operate as master)
     NSSMD1 = 0; NSSMD0 = 0; //Setting 3-wire mode.
     SPIEN = 1;              //Enable the SPI module.
-    XBR0 |= 0x02;           //Routing pins to SPI module via the crossbar.
-    P0MDOUT |= 0x45;
+    SFRPAGE = 0x0F;
+    XBR0 |= 0x03;           //Routing pins to SPI module via the crossbar.
+    XBR1 |= 0x20;
+    P0MDOUT |= 0x07;
+    P2MDOUT |= 0x02;
+    SFRPAGE = 0;
     return;
 }
 
 /*  returns the status of the CD signal from the microSD card adapter.
     It should return true (non-zero) when there is a card in the adapter. */
 uint8_t spicardpresent(){
-    return (uint8_t) CHKB(CD);        //Assuming is set high when card is in adapter.
+    return (uint8_t)CD;        //Assuming is set high when card is in adapter.
 }
 
 /*  The remaining procedures are called from the SD card module (note, these procedures all have
@@ -50,32 +60,43 @@ void spi_set_divisor(uint8_t spd){
 
 //asserts the CS chip select signal line (sets it low).
 void spi_cs_assert(){
-    CLRB(CS);
+    //CLRB(CS);
+    CS = 0;
     return;
 }
 
 //de-asserts the CS chip select signal line (sets it high).
 void spi_cs_deassert(){
-    SETB(CS);
+   // SETB(CS);
+    CS = 1;
     return;
 }
 
 //sends one byte over the SPI bus.
 void spi_send_byte(uint8_t input){
+    SPIF = 0;
     SPI0DAT = input;
+    while(!SPIF);
     return;
 }
 
 //receives one byte over the SPI bus
 uint8_t spi_rcv_byte(){
+    SPIF = 0;
+    SPI0DAT = 0xFF;
+    while(!SPIF);
     return SPI0DAT;
 }
 
 //sends multiple bytes (normally a SD card sector) over the SPI bus.  The code for this procedure was given earlier.
 void spi_rcv_buffer(uint16_t len, uint8_t xdata *buffer){
     int i;
+
+    spi_cs_assert();
     for(i = 0; i < len; i++)
-        SPI0DAT = buffer[i];
+        buffer[i] = spi_rcv_byte();
+    spi_cs_deassert();
+
     return;
 }
 
--- lab8/spi_test.c
+++ lab8/spi_test.c
@@ -6,30 +6,50 @@
 
 typedef struct {
       char string[16];
-      uint8_t len;
+      unsigned short len;
       uint8_t wavedata[512];
 } SD_data;
 
 int main(void){
     SD_data xdata dat;
     uint8_t lol;
+    uint8_t xdata buffer[512];
 
+    SFRPAGE = 0x0F;
     //init procedures
     WDTCN = 0xDE;             // Disable watchdog timer
     WDTCN = 0xAD;
     OSCICN = 0xC2;
+    XBR2 |= 0x40;              // Enable crossbar and weak pull-up
 
-    XBR2 = 0x40;              // Enable crossbar and weak pull-up
-
-
+    spi_set_divisor(0);
     spiinit();
     lcdinit();
 
-    while(!spicardpresent());
+    while(1){
+        while(!spicardpresent());
+        if(!microSDinit()){
+            lcdclear();
+            lcdpos(0,0);
+            lcdwrite("ERROR: microSD");
+            lcdpos(1,0);
+            lcdwrite("failure.");
+        } else {
+            microSDread(0x112358, buffer);
+            lcdclear();
+            lcdpos(0,0);
+            lcdwrite(buffer);
+            while(spicardpresent()){
+//              dat = (SD_data) buffer;
+
+                //output data to DAC
+            }
+        }
+    }
     lol = microSDinit();
 
     do {
-        lol = microSDread(0x11235813, dat.wavedata);
+        lol = microSDread(0x112358, dat.wavedata);
         lcdwrite(dat.string);
         lcdpos(0, 0);
     } while(!lol);
