de588bc41226b7d7677b34e137731f4c6da9450f
Aseda Gyeke Aboagye <aseda.aboagye@gmail.com>
2012-11-28 01:53:38 -0800

    Attempted to write playing a song code. To be debugged.

--- final/library/main.c
+++ final/library/main.c
@@ -5,14 +5,23 @@
 #include "sd.h"
 #include "dac.h"
 #include "readdir.h"
+#include "wav.h"
 
 //#pragma code                        // Include ASM in .LST file
 void kbinit ( void );
 uint8_t kbcheck ( void );
 void dacinit ( void );
 
+extern uint8_t  xdata numSongs;      // Number of songs found
+extern uint32_t xdata songSector[32];// Starting sector of each file.
+
 int main(void){
-    SD_data xdata dat;
+    wav_header header;
+    wav_header* header_ptr = &header;
+    int current_sector = songSector[0];
+    int i, ping, pong;
+    uint32_t bytestoplay, bytesread;
+    uint8_t xdata buffer[2][512];
 
     //init procedures
     SFRPAGE = 0x0F;
@@ -31,22 +40,22 @@ int main(void){
     dacstereo(0);                   // Set number of channels to mono
 
 /*
-	while(1) {
-		uint8_t temp[1];
-		PCON |= 1;
-		temp[0] = kbcheck();
-		if (temp[0] == 13) {
-			lcdclear();
+    while(1) {
+        uint8_t temp[1];
+        PCON |= 1;
+        temp[0] = kbcheck();
+        if (temp[0] == 13) {
+            lcdclear();
             lcdpos(0,0);
-		}
-		else if (temp[0] == 8) {
-			_lcdcmd(0x10);
-			lcdwrite(" ");
-			_lcdcmd(0x10);
-		}
-		else if (temp[0] != 255)
-			lcdwrite(temp);
-	}
+        }
+        else if (temp[0] == 8) {
+            _lcdcmd(0x10);
+            lcdwrite(" ");
+            _lcdcmd(0x10);
+        }
+        else if (temp[0] != 255)
+            lcdwrite(temp);
+    }
 */
 
     while(1){
@@ -59,18 +68,52 @@ int main(void){
             lcdwrite("failure.");
         } else {
             spi_set_divisor(1);     // Set to max speed after initialisation
-	
-			readdir();
-			lcdwrite("do");
+            readdir();              // Fill in numSongs and songSector[32]
 
-            microSDread(0x112358, (uint8_t *) &dat);
-            lcdclear();
-            lcdpos(0,0);
-            lcdwrite(dat.string);
             while(spicardpresent()){
-                PCON |= 1;
-                if(!dacbusy()){
-                    dacplay(dat.len, dat.wavedata); //output to DAC
+                PCON |= 1;          // Power management setting
+                for(i=0; i<numSongs; i++){
+                    current_sector = songSector[i];
+                    microSDread(current_sector, (uint8_t *) &header);
+                    switch(ntohs(header.numChannels)){
+                        case 2:
+                            dacstereo(1); // Stereo
+                        case 1:
+                        default:
+                            dacstereo(0); // Mono
+                    }
+                    dacrate(ntohl(header.sampleRate));
+                    bytestoplay = ntohl(header.subchunk2Size);
+                    lcdclear();
+                    lcdpos(0,0);
+                    lcdwrite(i); lcdwrite(":");
+                    lcdwrite(header.artist);
+                    lcdpos(1,0);
+                    lcdwrite(header.title);
+                    if(!dacbusy()){
+                        dacplay(404, (uint8_t xdata *)(header_ptr + 108));
+                        //RIFF header + artist&title strings out of 512 B block
+                    }
+                    bytesread = 404;
+                    bytestoplay -= 404;
+                    ping = 0;
+                    microSDread(current_sector + bytesread, buffer[ping]);
+                    // Well, we stop playing when bytestoplay = 0. If not, read
+                    while(bytestoplay > 0){
+                        pong = ping;
+                        ping = 1-pong;  // To keep ahead of the DAC
+                        if(!dacbusy()){
+                            if(bytestoplay >= 512){
+                                dacplay(512, buffer[pong]);
+                                bytestoplay -= 512;
+                                bytesread += 512;
+                            }else{
+                                dacplay(bytestoplay, buffer[pong]);
+                                bytestoplay = 0; // Done playing song.
+                            }
+                            microSDread(current_sector + bytesread, buffer[ping]);
+                        }
+                    }
                 }
             }
         }
