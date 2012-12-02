14acff0893d2c1dd6b7b886eae1db02fef2dfc9d
Joe Orr <joeorr4@gmail.com>
2012-12-01 17:25:34 -0800

    copied changes from library to main

--- final_code/main/dac.h
+++ final_code/main/dac.h
@@ -11,12 +11,4 @@ void dacinit(void);
 uint8_t dacbusy(void);
 void dacplay(uint16_t count, uint8_t xdata *buffer);
 
-// SFRs: 16-bit little endian. SFRL and SFRH
-sfr16 RCAP2  = 0xCA;
-sfr16 DACnCN = 0xD4; // DAC0: SFRPAGE 0, DAC1: SFRPAGE 1
-sfr16 DACn   = 0xD2;
-
-#define MAX_VOLUME 5
-#define MIN_VOLUME 0
-
 #endif
--- final_code/main/lcd.h
+++ final_code/main/lcd.h
@@ -8,6 +8,9 @@ void lcdinit();
 void lcdwrite(uint8_t *str);
 void lcdwritec(uint8_t code *str);
 void lcdwritex(uint8_t xdata *str);
+void lcdwritei8(uint8_t i);         // display an 8-bit number
+void lcdwritei16(uint16_t i);       // display a 16-bit number
+
 void lcdpos(uint8_t row, uint8_t col);
 void lcdcursor(uint8_t mode);
 void lcdclear();
